9eb5b21498d2c6bfa79633c32feb5f57f3fda2a7
irwinet <irwinestradatorres@gmail.com>
2020-11-20 12:18:26 -0500

    Save Token at LocalStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +11,40 @@ export class AuthService {
 
   private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
   private apiKey = 'AIzaSyCzSH1w7h0dibhXUENk7IhOZodVoFSfoqg';
-  
+  userToken: string;
+
   // Crear Nuevo Usuario
   // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
   // Login
   // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.leerToken();
+   }
 
   logout(){
     
   }
 
   login(usuario: UsuarioModel){
+    const authData = {
+      // email: usuario.email,
+      // password: usuario.password,
+      ...usuario,
+      returnSecureToken: true
+    }
 
+    return this.http.post(
+      `${this.url}:signInWithPassword?key=${this.apiKey}`,
+      authData
+    ).pipe(
+      map(resp=>{
+        console.log('Entro en el mapa del RXJS');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
+    );
   }
 
   nuevoUsuario(usuario: UsuarioModel){
@@ -37,6 +58,28 @@ export class AuthService {
     return this.http.post(
       `${this.url}:signUp?key=${this.apiKey}`,
       authData
+    ).pipe(
+      map(resp=>{
+        console.log('Entro en el mapa del RXJS');
+        this.guardarToken(resp['idToken']);
+        return resp;
+      })
     );
+  }
+
+  private guardarToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+  }
+
+  leerToken(){
+    if(localStorage.getItem('token')){
+      this.userToken = localStorage.getItem('token');
+    }
+    else{
+      this.userToken = '';
+    }
+
+    return this.userToken;
   }
 }
