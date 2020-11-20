255eb77b258e720c3f42b7e5eb07b144395d815f
irwinet <irwinestradatorres@gmail.com>
2020-11-20 15:22:21 -0500

    Add Package SweetAlert2

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,9 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel;
-
-  constructor(private auth: AuthService) { }
+  
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -21,13 +24,27 @@ export class LoginComponent implements OnInit {
   login(form: NgForm){
     if(form.invalid) { return; }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     // console.log(this.usuario);
     // console.log(form);
 
     this.auth.login(this.usuario).subscribe(resp=>{
       console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+      Swal.fire({
+        title: 'Error al autenticar',
+        icon: 'error',
+        text: err.error.error.message
+      });
     });
   }
 
